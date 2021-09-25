4bb60cf63f70f37e71bc09aae0c5072eccf88432
Stanley <wltrstnly@hotmail.com>
2021-09-25 15:02:56 -0600

    Addign local route

--- src/Constants.js
+++ src/Constants.js
@@ -1,5 +1,5 @@
 export const API_URL = 'http://awsinsuranceapi-env.eba-c3mjwceg.us-east-2.elasticbeanstalk.com'
 //export const JPA_API_URL = 'http://awsinsuranceapi-env.eba-c3mjwceg.us-east-2.elasticbeanstalk.com'
-export const JPA_API_URL = 'localhost:5000'
+export const JPA_API_URL = 'http://localhost:5000'
 
 
